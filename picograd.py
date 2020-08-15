8ac7fe2bbc9cf4b99b8a2ae82611b22b95b3d955
Max Estep <estepmax@maxs-mbp.home>
2020-08-14 20:45:12 -0700

    aoaisdfnsf

--- picograd.py
+++ picograd.py
@@ -35,12 +35,12 @@ def makeVarOperator(left,right,op,topological=False,ap_op=None):
 class History(object):
     def __init__(self,variable,history=None):
         if history is None:
-            self.history = [(variable,'variable')]
+            self.history = [(variable,'var')]
         else:
             self.history = history
 
 class Variable(History):
-    def __init__(self,value,grad_=1,name='',history=None):
+    def __init__(self,value,grad_=1.,name='',history=None):
         super(Variable,self).__init__(value,history)
         if isinstance(value,Variable):
             self.value = value.value 
@@ -54,12 +54,12 @@ class Variable(History):
     def __add__(self,other):
         variable_ = makeVarOperator(self,other,'+')()
         self.history.append((variable_,'+'))
-        return Variable(variable_,self.history)
+        return Variable(value=variable_,history=self.history)
         
     def __sub__(self,other):
         variable_ = makeVarOperator(self,other,'-')()
         self.history.append((variable_,'-'))
-        return Variable(variable_,self.history)
+        return Variable(value=variable_,history=self.history)
     
     def __mul__(self,other):
         ap_op = self.value * other.grad_ + self.grad_ * other.value
@@ -71,7 +71,7 @@ class Variable(History):
             ap_op=ap_op
         )()
         self.history.append((variable_,'*'))
-        return Variable(variable_,self.history)
+        return Variable(value=variable_,history=self.history)
 
     def __div__(self,other):
         ap_op = (self.grad_ * other.value - self.value * other.grad_) / (other.value * other.value)
@@ -83,7 +83,7 @@ class Variable(History):
             ap_op=ap_op
         )()
         self.history.append((variable_,'/'))
-        return Variable(variable_,self.history)
+        return Variable(value=variable_,history=self.history)
 
     def __pow__(self,n):
         variable_ = Variable(
@@ -92,7 +92,7 @@ class Variable(History):
             (self.name,)
         )
         self.history.append((variable_,'**'))
-        return Variable(variable_,self.history)
+        return Variable(value=variable_,history=self.history)
     
     def linear(self):
         variable_ = Variable(
@@ -101,7 +101,7 @@ class Variable(History):
             (self.name,)
         )
         self.history.append((variable_,'f_linear'))
-        return Variable(variable_,self.history)
+        return Variable(value=variable_,history=self.history)
     
     def relu(self):
         variable_ = Variable(
@@ -110,7 +110,7 @@ class Variable(History):
             (self.name,)
         )
         self.history.append((variable_,'f_relu'))
-        return Variable(variable_,self.history)
+        return Variable(value=variable_,history=self.history)
 
     def sigmoid(self):
         s = lambda x: 1 / (1.0 + math.exp(-1.0*x))
@@ -120,7 +120,7 @@ class Variable(History):
             (self.name,)
         )
         self.history.append((variable_,'f_sigmoid'))
-        return Variable(variable_,self.history)
+        return Variable(value=variable_,history=self.history)
     
     def sin(self):
         variable_ = Variable(
@@ -128,8 +128,8 @@ class Variable(History):
             math.cos(self.value),
             (self.name,)
         )
-        self.history.append((variable_,'f_cos'))
-        return Variable(variable_,self.history)
+        self.history.append((variable_,'f_sin'))
+        return Variable(value=variable_,history=self.history)
 
     def backward(self):
         grad = 1.0
@@ -139,4 +139,6 @@ class Variable(History):
 
 class Pico(Variable):
     def __init__(self,value,name=''):
-        super(Pico,self).__init__(value=value,name=name)
+        super(Pico,self).__init__(
+            Variable(value=value,name=name)
+        )
