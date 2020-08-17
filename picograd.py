98979078c91c9a599afa0443fcf5f20a40c760a4
Max Estep <estepmax@maxs-mbp.home>
2020-08-17 08:56:36 -0700

    cleaned up r-methods

--- picograd.py
+++ picograd.py
@@ -14,7 +14,7 @@ def makeVarOperator(left,right,op,topological=False,ap_op=None):
             variable = Variable(
                 operators[op] (left.value,right.value),
                 ap_op,
-                (left,right)
+                (left,right),
             )
         else:
             variable = Variable(
@@ -44,29 +44,39 @@ class Variable(History):
             self.grad = grad
             self.obj = obj
     
+    def __neg__(self):
+        variable = Variable(
+            -1 * self.value,
+            -1 * self.grad,
+            (self,'--'),
+            self.history
+        )
+        variable.history.append((variable,'--'))
+        return variable
+
     def __add__(self,other):
         other = other if isinstance(other,Variable) else Variable(other,grad=0.,obj='const')
         variable = makeVarOperator(self,other,'+')()
-        self.history.append((variable,'+'))
-        return Variable(value=variable,history=self.history)
+        variable.history = self.history
+        variable.history.append((variable,'+'))
+        return variable
     
     def __radd__(self,other):
         other = other if isinstance(other,Variable) else Variable(other,grad=0.,obj='const')
-        variable = makeVarOperator(other,self,'+')()
-        self.history.append((variable,'+'))
-        return Variable(value=variable,history=self.history)      
+        variable = other + self
+        return variable   
     
     def __sub__(self,other):
         other = other if isinstance(other,Variable) else Variable(other,grad=0.,obj='const')
         variable = makeVarOperator(self,other,'-')()
-        self.history.append((variable,'-'))
-        return Variable(value=variable,history=self.history)
+        variable.history = self.history 
+        variable.history.append((variable,'-'))
+        return variable
     
     def __rsub__(self,other):
         other = other if isinstance(other,Variable) else Variable(other,grad=0.,obj='const')
-        variable = makeVarOperator(other,self,'-')()
-        self.history.append((variable,'-'))
-        return Variable(value=variable,history=self.history)
+        variable = other - self
+        return variable
 
     def __mul__(self,other):
         other = other if isinstance(other,Variable) else Variable(other,grad=0.,obj='const')
@@ -78,21 +88,14 @@ class Variable(History):
             topological=True,
             ap_op=ap_op
         )()
-        self.history.append((variable,'*'))
-        return Variable(value=variable,history=self.history)
+        variable.history = self.history
+        variable.history.append((variable,'*'))
+        return variable
 
     def __rmul__(self,other):
         other = other if isinstance(other,Variable) else Variable(other,grad=0.,obj='const')
-        ap_op = other.value * self.grad + other.grad * self.value
-        variable = makeVarOperator(
-            left=other,
-            right=self,
-            op='*',
-            topological=True,
-            ap_op=ap_op
-        )()
-        self.history.append((variable,'*'))
-        return Variable(value=variable,history=self.history)
+        variable = other * self
+        return variable
 
     def __truediv__(self,other):
         other = other if isinstance(other,Variable) else Variable(other,grad=0.,obj='const')
@@ -104,76 +107,75 @@ class Variable(History):
             topological=True,
             ap_op=ap_op
         )()
-        self.history.append((variable,'/'))
-        return Variable(value=variable,history=self.history)
+        variable.history = self.history
+        variable.history.append((variable,'/'))
+        return variable
     
     def __rtruediv__(self,other):
         other = other if isinstance(other,Variable) else Variable(other,grad=0.,obj='const')
-        ap_op = (other.grad * self.value - other.value * self.grad) / (self.value * self.value)
-        variable = makeVarOperator(
-            left=other,
-            right=self,
-            op='/',
-            topological=True,
-            ap_op=ap_op
-        )()
-        self.history.append((variable,'/'))
-        return Variable(value=variable,history=self.history)
+        variable = other / self
+        return variable
 
     def __pow__(self,n):
         variable = Variable(
             self.value ** n,
             0.0 if n == 0.0 else n * self.value ** (n - 1) * self.grad,
-            (self.obj,self)
+            (self.obj,self),
+            self.history
         )
-        self.history.append((variable,'**'))
-        return Variable(value=variable,history=self.history)
+        variable.history.append((variable,'**'))
+        return variable
     
     def linear(self):
         variable = Variable(
             self.value, 
             self.grad,
-            (self.obj,self)
+            (self.obj,self),
+            self.history
         )
-        self.history.append((variable,'f_linear'))
-        return Variable(value=variable,history=self.history)
+        variable.history.append((variable,'f_linear'))
+        return variable
     
     def relu(self):
         variable = Variable(
             0.0 if self.value < 0.0 else self.value,
             0.0 if self.value < 0.0 else 1.0,
-            (self.obj,self)
+            (self.obj,self),
+            self.history
         )
-        self.history.append((variable,'f_relu'))
-        return Variable(value=variable,history=self.history)
+        variable.history.append((variable,'f_relu'))
+        return variable
 
     def sigmoid(self):
         s = lambda x: 1 / (1.0 + math.exp(-1.0*x))
         variable = Variable(
             s(self.value),
             s(self.value)*(1.0-s(self.value)),
-            (self.obj,self)
+            (self.obj,self),
+            self.history
         )
-        self.history.append((variable,'f_sigmoid'))
-        return Variable(value=variable,history=self.history)
+        variable.history.append((variable,'f_sigmoid'))
+        return variable
     
     def sin(self):
         variable = Variable(
             math.sin(self.value),
             math.cos(self.value) * self.grad,
-            (self.obj,self)
+            (self.obj,self),
+            self.history
         )
-        self.history.append((variable,'f_sin'))
-        return Variable(value=variable,history=self.history)
+        variable.history.append((variable,'f_sin'))
+        return variable
 
     def exp(self):
         variable = Variable(
             math.exp(self.value),
             math.exp(self.value) * self.grad,
-            (self.obj,self)
+            (self.obj,self),
+            self.history
         )
-        self.history.append((variable,'f_exp'))
-        return Variable(value=variable,history=self.history)
+        variable.history.append((variable,'f_exp'))
+        return variable
     
     def backward(self):
         grad = 1.0
